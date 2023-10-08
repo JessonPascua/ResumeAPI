2a7663e8b1d9bf68a39354c2c1f856d75aa47350
JessonWork <jessonpascua.work@gmail.com>
2023-10-08 19:49:41 +0800

    Update Certifications.cs

--- ResumeAPI/Models/Certifications.cs
+++ ResumeAPI/Models/Certifications.cs
@@ -9,16 +9,16 @@ namespace ResumeAPI.Models
         [Key]
         public Guid CertificationId { get; set; }
 
-        [Column(TypeName = "VARCHAR(225)")]
+        [Column(TypeName = "VARCHAR(525)")]
         public string Name { get; set; }
         
-        [Column(TypeName = "VARCHAR(50)")]
+        [Column(TypeName = "VARCHAR(100)")]
         public string Issuer { get; set; }
 
         [Column(TypeName = "Date")]
         public DateTime IssueDate { get; set; }
 
-        [Column(TypeName = "VARCHAR(100)")]
+        [Column(TypeName = "VARCHAR(225)")]
         public string CredentialURL { get; set; }
 
         public DateTime Created_at { get; set; }
