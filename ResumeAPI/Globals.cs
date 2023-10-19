2b42643e532acef50ed126a78eccef67a8e5e919
JessonWork <jessonpascua.work@gmail.com>
2023-10-19 19:30:28 +0800

    Create Globals.cs

---
+++ ResumeAPI/Globals.cs
@@ -0,0 +1,7 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.Text.Json.Serialization;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using ResumeAPI.Data;
+global using ResumeAPI.Models;
