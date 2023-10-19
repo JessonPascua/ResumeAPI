56647c7aa67d89d48a95dbe642aa5e5aa6c6381a
JessonWork <jessonpascua.work@gmail.com>
2023-10-19 19:30:17 +0800

    Create ProjectController.cs

---
+++ ResumeAPI/Controllers/ProjectController.cs
@@ -0,0 +1,27 @@
+﻿namespace ResumeAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly ResumeAPIContext _context;
+
+        public ProjectController(ResumeAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("Projects")]
+        public async Task<ActionResult<IEnumerable<Projects>>> GetProjects()
+        {
+            var projectList = await _context.Projects.ToListAsync();
+
+            if (projectList == null || projectList.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(projectList);
+        }
+    }
+}
