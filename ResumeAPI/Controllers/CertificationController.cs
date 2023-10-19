67ec925b0a07226bbd6a3d5b98c53f75da01092b
JessonWork <jessonpascua.work@gmail.com>
2023-10-19 19:30:04 +0800

    Create CertificationController.cs

---
+++ ResumeAPI/Controllers/CertificationController.cs
@@ -0,0 +1,27 @@
+﻿namespace ResumeAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CertificationController : ControllerBase
+    {
+        private readonly ResumeAPIContext _context;
+
+        public CertificationController(ResumeAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("Certifications")]
+        public async Task<ActionResult<IEnumerable<Certifications>>> GetCertifications()
+        {
+            var certificationList = await _context.Certifications.ToListAsync();
+
+            if (certificationList == null || certificationList.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(certificationList);
+        }
+    }
+}
