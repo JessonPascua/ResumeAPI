23f0bf2ed41a777fad352490eb0ceb57af33bc2b
JessonWork <jessonpascua.work@gmail.com>
2023-10-19 19:30:13 +0800

    Create EducationController.cs

---
+++ ResumeAPI/Controllers/EducationController.cs
@@ -0,0 +1,27 @@
+﻿namespace ResumeAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class EducationController : ControllerBase
+    {
+        private readonly ResumeAPIContext _context;
+
+        public EducationController(ResumeAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("Educations")]
+        public async Task<ActionResult<IEnumerable<Educations>>> GetEducations()
+        {
+            var educationList = await _context.Educations.ToListAsync();
+
+            if (educationList == null || educationList.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(educationList);
+        }
+    }
+}
