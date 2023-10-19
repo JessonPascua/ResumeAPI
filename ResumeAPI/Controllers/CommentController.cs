d989c24d62d9efb2a6d77961befc4a1937d7c4c5
JessonWork <jessonpascua.work@gmail.com>
2023-10-19 19:30:07 +0800

    Create CommentController.cs

---
+++ ResumeAPI/Controllers/CommentController.cs
@@ -0,0 +1,41 @@
+﻿namespace ResumeAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ResumeAPIContext _context;
+
+        public CommentController(ResumeAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("Comments")]
+        public async Task<ActionResult<IEnumerable<Comments>>> GetComments()
+        {
+            var commentList = await _context.Comments.ToListAsync();
+
+            if (commentList == null || commentList.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(commentList);
+        }
+
+        //[HttpPost]
+        //[Route("PostComments")]
+        //public async Task<ActionResult<Resume>> PostComment([Bind("name")] Comments comment)
+        //{
+        //    if (_context.Comments == null)
+        //    {
+        //        return Problem("Entity set 'ResumeAPIContext.Comments'  is null.");
+        //    }
+        //    _context.Comments.Add(comment);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetComments", new { id = comment.CommentId }, comment);
+        //}
+    }
+}
