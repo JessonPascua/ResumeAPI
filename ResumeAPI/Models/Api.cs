8160f0407a83fad32deb585d2f918a72ac69be15
JessonWork <jessonpascua.work@gmail.com>
2023-10-19 19:30:41 +0800

    Create Api.cs

---
+++ ResumeAPI/Models/Api.cs
@@ -0,0 +1,10 @@
+﻿namespace ResumeAPI.Models
+{
+    public class Api
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string[] routes { get; set; }
+        public virtual List<Routes> Routes { get; set; }
+    }
+}
