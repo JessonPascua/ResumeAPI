fe47fd10f2c104d0170c5944193ae92d5d08c7a7
JessonWork <jessonpascua.work@gmail.com>
2023-10-19 19:30:10 +0800

    Create ContactController.cs

---
+++ ResumeAPI/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿namespace ResumeAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly ResumeAPIContext _context;
+
+        public ContactController(ResumeAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("Contacts")]
+        public async Task<ActionResult<IEnumerable<Contacts>>> GetContacts()
+        {
+            var contactList = await _context.Contacts.ToListAsync();
+
+            if (contactList == null || contactList.Count == 0)
+            {
+                return NotFound();
+            }
+            return Ok(contactList);
+        }
+    }
+}
