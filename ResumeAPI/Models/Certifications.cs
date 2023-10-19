7d778f214b6de6f012805920eedec43513c44f6e
JessonWork <jessonpascua.work@gmail.com>
2023-10-19 19:30:44 +0800

    Update Certifications.cs

--- ResumeAPI/Models/Certifications.cs
+++ ResumeAPI/Models/Certifications.cs
@@ -1,8 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
-
-namespace ResumeAPI.Models
+﻿namespace ResumeAPI.Models
 {
     public class Certifications
     {
@@ -21,14 +17,14 @@ namespace ResumeAPI.Models
         [Column(TypeName = "VARCHAR(225)")]
         public string CredentialURL { get; set; }
 
-        public DateTime Created_at { get; set; }
+        //public DateTime Created_at { get; set; }
 
-        // Foreign key for Resume
-        public Guid ResumeId { get; set; }
+        //// Foreign key for Resume
+        //public Guid ResumeId { get; set; }
 
-        // Navigation property
-        [ForeignKey("ResumeId")]
-        [JsonIgnore]
-        public virtual Resume Resume { get; set; }
+        //// Navigation property
+        //[ForeignKey("ResumeId")]
+        //[JsonIgnore]
+        //public virtual Resume Resume { get; set; }
     }
 }
