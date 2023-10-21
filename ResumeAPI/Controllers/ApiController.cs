527500ed6d765ebbf90036864775f4ebcfc40ed5
JessonWork <jessonpascua.work@gmail.com>
2023-10-21 21:03:14 +0800

    Update ApiController.cs

--- ResumeAPI/Controllers/ApiController.cs
+++ ResumeAPI/Controllers/ApiController.cs
@@ -14,20 +14,16 @@ namespace ResumeAPI.Controllers
             _context = context;
         }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Api>>> GetAllRoutes()
-        {
-            if (_context.Api == null)
-            {
-                return NotFound();
-            }
-            var routes = await _context.Api.Include(x => x.Routes).ToListAsync();
-            var result = routes.Select(m => new
-            {
-                m.Id,
-                Routes = m.Routes.Select(r => new { r.Description, r.Url })
-            });
-            return Ok(result);
-        }
+        //[HttpGet]
+        //public async Task<ActionResult<IEnumerable<Api>>> GetAllRoutes()
+        //{
+        //    if (_context.Api == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var routes = await _context.Api.Include(x => x.Routes).ToListAsync();
+
+        //    return Ok(routes);
+        //}
     }
 }
