4b125fcbacd1144e3b93d355d5e5e7238d6b98f1
JessonWork <jessonpascua.work@gmail.com>
2023-10-21 21:03:51 +0800

    Create RoutesList.cs

---
+++ ResumeAPI/Models/RoutesList.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResumeAPI
+{
+    public class RoutesList
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+
+        public string ApiId { get; set; }
+        public Api Api { get; set; }
+    }
+}
