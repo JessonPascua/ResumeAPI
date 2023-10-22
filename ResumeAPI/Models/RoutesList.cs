0b5f78abbb61f93cbd5cebc07afcd4e86be1d046
JessonWork <jessonpascua.work@gmail.com>
2023-10-22 16:20:04 +0800

    Update RoutesList.cs

--- ResumeAPI/Models/RoutesList.cs
+++ ResumeAPI/Models/RoutesList.cs
@@ -6,11 +6,15 @@ namespace ResumeAPI
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
         public Guid Id { get; set; }
         public string Description { get; set; }
         public string Url { get; set; }
 
+        [JsonIgnore]
         public string ApiId { get; set; }
+
+        [JsonIgnore]
         public Api Api { get; set; }
     }
 }
