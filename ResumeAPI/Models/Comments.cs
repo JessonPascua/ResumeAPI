57d3786bcad8b0b02a750e0d0a03fad2fa3933dd
JessonWork <jessonpascua.work@gmail.com>
2023-10-22 16:19:48 +0800

    Update Comments.cs

--- ResumeAPI/Models/Comments.cs
+++ ResumeAPI/Models/Comments.cs
@@ -7,24 +7,30 @@ namespace ResumeAPI.Models
     public class Comments
     {
         [Key]
+        [JsonIgnore]
         public Guid CommentId { get; set; }
 
+        [Required]
         [Column(TypeName = "VARCHAR(50)")]
         public string Name { get; set; }
         
+        [Required]
         [Column(TypeName = "VARCHAR(50)")]
         public string Email { get; set; }
 
+        [Required]
         [Column(TypeName = "TEXT")]
         public string Massage { get; set; }
-        public DateTime Created_at { get; set; }
 
-        //// Foreign key for Resume
-        //public Guid ResumeId { get; set; }
+        public DateTime? Created_at { get; set; }
 
-        //// Navigation property
-        //[ForeignKey("ResumeId")]
-        //[JsonIgnore]
-        //public virtual Resume Resume { get; set; }
+        // Foreign key for Resume
+        [JsonIgnore]
+        public Guid ResumeId { get; set; }
+
+        // Navigation property
+        [ForeignKey("ResumeId")]
+        [JsonIgnore]
+        public virtual Resume? Resume { get; set; }
     }
 }
