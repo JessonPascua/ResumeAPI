b215d2e66cc018d934a098ba7900a0707b8b75fc
JessonWork <jessonpascua.work@gmail.com>
2023-11-11 21:22:40 +0800

    Create ApiController.cs

---
+++ ResumeAPIwithSupabase/Controllers/ApiController.cs
@@ -0,0 +1,30 @@
+﻿//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using ResumeAPIwithSupabase.Models;
+
+//namespace ResumeAPI.Controllers
+//{
+//    [Route("api")]
+//    [ApiController]
+//    public class ApiController : ControllerBase
+//    {
+//        private readonly Supabase.Client _context;
+
+//        public ApiController(Supabase.Client context)
+//        {
+//            _context = context;
+//        }
+
+//        [HttpGet]
+//        public async Task<ActionResult<IEnumerable<Api>>> GetAllRoutes()
+//        {
+//            if (_context.Api == null)
+//            {
+//                return NotFound();
+//            }
+//            var routes = await _context.Api.Include(x => x.RoutesList).ToListAsync();
+
+//            return Ok(routes);
+//        }
+//    }
+//}
