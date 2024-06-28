cd73e6bb7d64331ee7c8db9bef39beab4b71d7cc
JessonWork <jessonpascua.work@gmail.com>
2024-06-28 23:38:36 +0800

    Create Educations.cs

---
+++ ResumeAPIwithSupabase/Models/Educations.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace ResumeAPIwithSupabase.Models
+{
+    public class Educations
+    {
+        [Key]
+        public Guid EducationId { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string School { get; set; }
+        
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Degree { get; set; }
+
+        [Column(TypeName = "TEXT")]
+        public string Description { get; set; }
+
+        public DateTime Created_at { get; set; }
+
+        //Foreign key for Resume
+        public Guid ResumeId { get; set; }
+
+        // Navigation property
+        [ForeignKey("ResumeId")]
+        [JsonIgnore]
+        public virtual Resume Resume { get; set; }
+    }
+}
