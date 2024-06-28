11a73ff63bf1a2e1725cce6cb616b9a7f2eceaae
JessonWork <jessonpascua.work@gmail.com>
2024-06-28 23:38:30 +0800

    Create Projects.cs

---
+++ ResumeAPIwithSupabase/Models/Projects.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResumeAPIwithSupabase.Models
+{
+    public class Projects
+    {
+        [Key]
+        public Guid ProjectId { get; set; }
+        
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Name { get; set; }
+        
+        [Column(TypeName = "VARCHAR(100)")]
+        public string? Repo { get; set; }
+       
+        [Column(TypeName = "VARCHAR(100)")]
+        public string? Live { get; set; }
+        
+        [Column(TypeName = "TEXT")]
+        public string? Description { get; set; }
+
+        [Column(TypeName = "TEXT[]")]
+        public string[]? Stacks { get; set; }
+
+        public DateTime? Created_at { get; set; }
+
+        // Foreign key for Resume
+        [JsonIgnore]
+        public Guid ResumeId { get; set; }
+
+        // Navigation property
+        [ForeignKey("ResumeId")]
+        [JsonIgnore]
+        public virtual Resume Resume { get; set; }
+    }
+}
