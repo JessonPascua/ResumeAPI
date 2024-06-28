00734e7526ec955cb4aa7f33be6065abcec1e1d1
JessonWork <jessonpascua.work@gmail.com>
2024-06-28 23:38:22 +0800

    Create RoutesList.cs

---
+++ ResumeAPIwithSupabase/Models/RoutesList.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using Postgrest.Attributes;
+using Postgrest.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResumeAPIwithSupabase.Models
+{
+    public class RoutesList : BaseModel
+    {
+        [PrimaryKey("id", false)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+
+        [JsonIgnore]
+        public string ApiId { get; set; }
+
+        [JsonIgnore]
+        public Api Api { get; set; }
+    }
+}
