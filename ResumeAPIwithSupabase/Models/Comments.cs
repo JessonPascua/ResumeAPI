267084961849c413aaf8a12d2b69e4b6bfefda82
JessonWork <jessonpascua.work@gmail.com>
2024-06-28 23:38:43 +0800

    Create Comments.cs

---
+++ ResumeAPIwithSupabase/Models/Comments.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace ResumeAPIwithSupabase.Models
+{
+    public class Comments
+    {
+        [Key]
+        [JsonIgnore]
+        public Guid CommentId { get; set; }
+
+        [Required]
+        [Column(TypeName = "VARCHAR(50)")]
+        public string Name { get; set; }
+        
+        [Required]
+        [Column(TypeName = "VARCHAR(50)")]
+        public string Email { get; set; }
+
+        [Required]
+        [Column(TypeName = "TEXT")]
+        public string Massage { get; set; }
+
+        public DateTime? Created_at { get; set; }
+
+        // Foreign key for Resume
+        [JsonIgnore]
+        public Guid ResumeId { get; set; }
+
+        // Navigation property
+        [ForeignKey("ResumeId")]
+        [JsonIgnore]
+        public virtual Resume? Resume { get; set; }
+    }
+}
