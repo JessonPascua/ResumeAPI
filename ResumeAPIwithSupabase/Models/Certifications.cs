52e20cc949ca1b60e1420e0ed8f67ca9190dd4be
JessonWork <jessonpascua.work@gmail.com>
2024-06-28 23:38:46 +0800

    Create Certifications.cs

---
+++ ResumeAPIwithSupabase/Models/Certifications.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResumeAPIwithSupabase.Models
+{
+    public class Certifications
+    {
+        [Key]
+        public Guid CertificationId { get; set; }
+
+        [Column(TypeName = "VARCHAR(525)")]
+        public string Name { get; set; }
+        
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Issuer { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateTime IssueDate { get; set; }
+
+        [Column(TypeName = "VARCHAR(225)")]
+        public string CredentialURL { get; set; }
+
+        public DateTime? Created_at { get; set; }
+
+        // Foreign key for Resume
+        [JsonIgnore]
+        public Guid ResumeId { get; set; }
+
+        // Navigation property
+        [ForeignKey("ResumeId")]
+        [JsonIgnore]
+        public virtual Resume Resume { get; set; }
+    }
+}
