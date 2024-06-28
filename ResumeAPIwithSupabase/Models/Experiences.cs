8fac1bd527ba300b23ab8d6fb0d390ad75140ba1
JessonWork <jessonpascua.work@gmail.com>
2024-06-28 23:38:33 +0800

    Create Experiences.cs

---
+++ ResumeAPIwithSupabase/Models/Experiences.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace ResumeAPIwithSupabase.Models
+{
+    public class Experiences
+    {
+        [Key]
+        public Guid ExperienceId { get; set; }
+
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Title { get; set; }
+        
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Company { get; set; }
+        
+        [Column(TypeName = "VARCHAR(100)")]
+        public string Range { get; set; }
+
+        [Column(TypeName = "TEXT[]")]
+        public string[]? Responsibilities { get; set; }
+        public DateTime? Created_at { get; set; }
+
+        // Foreign key for Resume
+        [JsonIgnore]
+        public Guid ResumeId { get; set; }
+
+        // Navigation property
+        [ForeignKey("ResumeId")]
+        [JsonIgnore]
+        public virtual Resume Resume { get; set; }
+    }
+}
