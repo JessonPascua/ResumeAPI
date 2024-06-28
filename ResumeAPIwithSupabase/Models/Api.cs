400b58c5f8e75adc7767c884fd03388bd7136c23
JessonWork <jessonpascua.work@gmail.com>
2024-06-28 23:38:49 +0800

    Create Api.cs

---
+++ ResumeAPIwithSupabase/Models/Api.cs
@@ -0,0 +1,12 @@
+﻿using Postgrest.Attributes;
+using Postgrest.Models;
+
+namespace ResumeAPIwithSupabase.Models;
+
+[Table("api")]
+public class Api : BaseModel
+{
+    [PrimaryKey("id", false)]
+    public string Id { get; set; }
+    public List<RoutesList> RoutesList { get; set; }
+}
